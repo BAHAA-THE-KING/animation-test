bb092941b6ed74f7f422cbd2896f10b5585787cc
Hamza saad aldeen <hamzasaadaldeen2002@gmail.com>
2023-10-18 18:43:15 +0300

    fff

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -16,7 +16,7 @@ export default function NavBar() {
 
   return(
     <>
-    <div className={navbar ? "container-fluid active" : "contianer-fluid"} style={{zIndex: "10", position: "fixed", width:"100%"}}>
+    <div className={navbar ? "container-fluid actv" : "contianer-fluid"} style={{zIndex: "10", position: "fixed", width:"100%"}}>
       <div className="navbar d-flex justify-content-between align-items-center" >
         <div className="logo d-flex justify-content-center align-items-center" style={{width:"140px", height:"100px"}}><img className="img-fluid" src={'assets/logo small.png'} alt="" /></div>
         <button className=" sign rounded-pill bg-light pt-1 pb-1 ps-4 pe-4 fw-bold fs-7" style={{border:"none", outline:"none",cursor:"pointer"}}>Sign up</button>
