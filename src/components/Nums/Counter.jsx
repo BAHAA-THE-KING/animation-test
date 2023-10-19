9381af3e7554665fb5b7aba272daab9c6f50167a
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-10-19 22:47:43 +0300

    Finish Animaitons.

--- src/components/Nums/Counter.jsx
+++ src/components/Nums/Counter.jsx
@@ -18,7 +18,7 @@ function Counter({ value }) {
                         val => {
                            if (val >= value) clearInterval(inter.current);
                            return Math.min(
-                              (val >= value - (value * 1 / 10)) ?
+                              (val >= (value - (value * 1 / 10))) ?
                                  (val + (value * 1 / 10 / 75)) :
                                  (val + ((value - (value * 1 / 10)) / (1000 / 30))),
                               value
@@ -26,7 +26,7 @@ function Counter({ value }) {
                         });
                   }, 30);
             }
-            if (window.scrollY >= 2500) { setFade(true); }
+            if (window.scrollY >= 2222) { setFade(true); }
             else if (window.scrollY < 2200) { setFade(false); }
          }
          document.addEventListener("scroll", runAnim);
@@ -37,27 +37,42 @@ function Counter({ value }) {
 
    return (
       <div
-         className={`
+         style={{
+            width: "20%",
+            aspectRatio: "1",
+            marginTop: "400px",
+            marginBottom: "400px",
+         }}
+      >
+         <div
+            className={`
                     position-relative
                     text-center
                     ${fade ? "ctrs" : ""}
                     `}
-         style={{
-            width: "20%",
-            aspectRatio: "1",
-            marginTop: "450px",
-            marginBottom: "400px",
-            fontSize: "65px",
-            border: "10px rgb(51 65 85) solid",
-            borderRadius: "50%",
-            backgroundColor: "white",
-            fontWeight: "bold",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-         }}>
-         <div className="ctr" style={{ transition: "0.5s", position: "absolute", top: "70%", opacity: "0.1" }}>
-            {Math.round(cValue)}
+            style={{
+               width: "100%",
+               height: "100%",
+               fontSize: "85px",
+               border: "10px rgb(51 65 85) solid",
+               borderRadius: "50%",
+               backgroundColor: "white",
+               fontWeight: "bold",
+               display: "flex",
+               justifyContent: "center",
+               alignItems: "center"
+            }}>
+            <div className="ctr" style={{ transition: "0.5s", position: "absolute", top: "70%", opacity: "0.1" }}>
+               {Math.round(cValue)}
+            </div>
+         </div>
+         <div
+            style={{
+               fontSize: "40px",
+               color: "white"
+            }}
+         >
+            Songs
          </div>
       </div>
    );
--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -32,38 +32,69 @@ function OurServices() {
 
 
    return (
-      <motion.div ref={ref} className="w-100 h-100 d-flex flex-wrap flex-row justify-content-evenly"
+      <motion.div
+         ref={ref}
+         className="w-100 h-100 d-flex flex-wrap flex-row justify-content-evenly"
          variants={serviceVariant}
          initial="hidden"
          animate={controler}
       >
          {
-            ["", "", "", "", "", ""].map(
+            ["service", "service", "service", "service", "service", "service"].map(
                (e, i) =>
-                  <div
-                     key={i}
-                     className="
+                  <div>
+                     <div
+                        key={e + (i + 1)}
+                        className="
                                 position-relative
                                 text-center
                                 nota
                                 "
-                     style={{
-                        width: "250px",
-                        height: "250px",
-                        margin: "3rem 5rem",
-                        border: "10px rgb(51 65 85) solid",
-                        borderRadius: "50%",
-                        backgroundColor: "white",
-                        backgroundImage: "url('assets/Landing (web) - 1-image5.png')",
-                        backgroundSize: "65% 65%",
-                        backgroundPosition: "37px 37px",
-                        backgroundRepeat: "no-repeat",
-                        filter: "drop-shadow(-50px 0 6px #AAA)",
-                        transition: "0.2s"
-                     }}
-                  >
-                     <div>
-                        {e}
+                        style={{
+                           width: "250px",
+                           height: "250px",
+                           margin: "3rem 5rem",
+                           border: "10px rgb(51 65 85) solid",
+                           backgroundColor: "white",
+                           borderRadius: "50%",
+                           filter: "drop-shadow(-50px 0 6px #AAA)",
+                           transition: "0.2s",
+                           display: "flex",
+                           flexDirection: "column",
+                           justifyContent: "center",
+                           alignItems: "center"
+                        }}
+                     >
+                        <div
+                           style={{
+                              backgroundColor: "transparent",
+                              backgroundImage: "url('assets/Landing (web) - 1-image5.png')",
+                              backgroundSize: "100% 100%",
+                              backgroundRepeat: "no-repeat",
+                              width: "65%",
+                              height: "65%",
+                              transition: "0.2s"
+                           }}
+                        ></div>
+                        <div
+                           style={{
+                              backgroundColor: "transparent",
+                              backgroundImage: "url('assets/Landing (web) - 1-image5 - Copy.png')",
+                              backgroundSize: "100% 100%",
+                              backgroundRepeat: "no-repeat",
+                              width: "65%",
+                              height: "0",
+                              transition: "0.2s"
+                           }}
+                        ></div>
+                     </div>
+                     <div
+                        style={{
+                           marginTop: "-30px",
+                           fontSize: "30px"
+                        }}
+                     >
+                        {e + (i + 1)}
                      </div>
                   </div>
             )
